df71ebad022d9a36c07da69678d74d8bd7eb1f16
TheLuckyman30 <bnauta0@gmail.com>
2026-02-04 23:08:12 -0500

    Added choice selection

--- questions.js
+++ questions.js
@@ -1,21 +1,32 @@
 const questions = [
   {
     question: "Is it called football or soccer?",
-    choices: ["Football", "Football"],
+    choices: [
+      { text: "Football", correct: true },
+      { text: "Football", correct: true },
+    ],
+    selectedChoiceId: "",
+    answered: false,
+  },
+  {
+    question: "Is it called football or soccer?",
+    choices: [
+      { text: "Soccer", correct: true },
+      { text: "Soccer", correct: true },
+    ],
+    selectedChoiceId: "",
+    answered: false,
   },
 ];
 
+const titleSection = document.getElementById("title");
+const questionSection = document.getElementById("question");
+const questionTitle = document.getElementById("question-title");
+const questionChoiceSection = document.getElementById("question-choices");
+const prevButton = document.getElementById("prev-page");
+const nextButton = document.getElementById("next-page");
 let currentPage = -1;
 
-const nextButton = document.getElementById("next-page");
-nextButton.addEventListener("click", () => {
-  if (currentPage < questions.length - 1) {
-    currentPage++;
-    updatePage();
-  }
-});
-
-const prevButton = document.getElementById("prev-page");
 prevButton.addEventListener("click", () => {
   if (currentPage !== -1) {
     currentPage--;
@@ -23,30 +34,59 @@ prevButton.addEventListener("click", () => {
   }
 });
 
+nextButton.addEventListener("click", () => {
+  if (currentPage < questions.length - 1) {
+    currentPage++;
+    updatePage();
+  }
+});
+
 function updatePage() {
   if (currentPage > -1) {
-    document.getElementById("title").hidden = true;
-    document.getElementById("question").hidden = false;
+    titleSection.hidden = true;
+    questionSection.hidden = false;
     prevButton.hidden = false;
     const currentQuestion = questions[currentPage];
 
     if (currentQuestion) {
-      document.getElementById("question-title").textContent =
-        currentQuestion.question;
+      questionTitle.textContent = currentQuestion.question;
 
-      const choiceSection = document.getElementById("question-choices");
-      choiceSection.innerHTML = "";
-      for (const choice of currentQuestion.choices) {
+      const controller = new AbortController();
+      const { signal } = controller;
+      questionChoiceSection.innerHTML = "";
+      for (const [index, choice] of currentQuestion.choices.entries()) {
         const newChoice = document.createElement("div");
-        const text = document.createTextNode(choice);
+        const text = document.createTextNode(choice.text);
         newChoice.appendChild(text);
-        newChoice.className = "choice";
-        choiceSection.appendChild(newChoice);
+        newChoice.id = `choice${index}`;
+        newChoice.className =
+          currentQuestion.selectedChoiceId === newChoice.id
+            ? "choice selected-choice"
+            : "choice";
+
+        if (!currentQuestion.selectedChoiceId) {
+          newChoice.addEventListener(
+            "click",
+            () => selectChoice(newChoice.id, currentQuestion, controller),
+            {
+              signal,
+            },
+          );
+        }
+
+        questionChoiceSection.appendChild(newChoice);
       }
     }
   } else {
-    document.getElementById("title").hidden = false;
+    titleSection.hidden = false;
+    questionSection.hidden = true;
     prevButton.hidden = true;
-    document.getElementById("question").hidden = true;
   }
+}
+
+function selectChoice(id, currentQuestion, controller) {
+  const selectedElement = document.getElementById(id);
+  selectedElement.className = "choice selected-choice";
+  currentQuestion.selectedChoiceId = selectedElement.id;
+  controller.abort();
 }
